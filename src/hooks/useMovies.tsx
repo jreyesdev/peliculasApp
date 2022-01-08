137cd4cf341b2bc37954b8d5b3ce772248bfe36c
jreyesdev <djrm1204@gmail.com>
2022-01-08 01:30:36 -0400

    Mejora en peticion de peliculas y estado

--- src/hooks/useMovies.tsx
+++ src/hooks/useMovies.tsx
@@ -2,22 +2,44 @@ import {useEffect, useState} from 'react';
 import movieDB from '../api/movieDB';
 import {Movie, MovieDbResponse} from '../interfaces/MovieDBInterface';
 
+interface MoviesState {
+  nowPlaying: Movie[];
+  popular: Movie[];
+  topRated: Movie[];
+  upComing: Movie[];
+}
+
 export const useMovies = () => {
   const [loading, setLoading] = useState<Boolean>(true);
-  const [peliculasActuales, setPeliculasActuales] = useState<Movie[]>([]);
+  const [moviesState, setMovies] = useState<MoviesState>({
+    nowPlaying: [],
+    popular: [],
+    topRated: [],
+    upComing: [],
+  });
 
-  const getMoviesNow = async () => {
-    const resp = await movieDB.get<MovieDbResponse>('/now_playing');
-    setPeliculasActuales(resp.data.results);
+  const getMovies = async () => {
+    const resp = await Promise.all([
+      movieDB.get<MovieDbResponse>('/now_playing'),
+      movieDB.get<MovieDbResponse>('/popular'),
+      movieDB.get<MovieDbResponse>('/top_rated'),
+      movieDB.get<MovieDbResponse>('/upcoming'),
+    ]);
+    setMovies({
+      nowPlaying: resp[0].data.results,
+      popular: resp[1].data.results,
+      topRated: resp[2].data.results,
+      upComing: resp[3].data.results,
+    });
     setLoading(false);
   };
 
   useEffect(() => {
-    getMoviesNow();
+    getMovies();
   }, []);
 
   return {
+    ...moviesState,
     loading,
-    peliculasActuales,
   };
 };
