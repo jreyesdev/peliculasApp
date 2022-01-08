0168320ffe21a81c786cab858ac27b9eb0138c26
jreyesdev <djrm1204@gmail.com>
2022-01-08 01:33:18 -0400

    Pantalla home

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Dimensions, ScrollView, StyleSheet, View} from 'react-native';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Carousel from 'react-native-snap-carousel';
+
+import HorizontalSlider from '../components/HorizontalSlider';
+import LoadingSpin from '../components/LoadingSpin';
+import MoviePoster from '../components/MoviePoster';
+import {useMovies} from '../hooks/useMovies';
+
+const {width: windowWidth} = Dimensions.get('window');
 
 const HomeScreen = () => {
+  const {top} = useSafeAreaInsets();
+  const {loading, nowPlaying, popular, topRated, upComing} = useMovies();
+
+  if (loading) {
+    return <LoadingSpin />;
+  }
+
   return (
-    <View>
-      <Text>Home screen</Text>
-    </View>
+    <ScrollView>
+      <View
+        style={{
+          ...styles.container,
+          marginTop: top + 10,
+        }}>
+        <View style={styles.carouselContainer}>
+          <Carousel
+            data={nowPlaying}
+            itemWidth={250}
+            inactiveSlideOpacity={0.8}
+            renderItem={({item}: any) => <MoviePoster movie={item} />}
+            sliderWidth={windowWidth}
+          />
+        </View>
+        <HorizontalSlider title="Popular" movies={popular} />
+        <HorizontalSlider title="Top Rated" movies={topRated} />
+        <HorizontalSlider title="Up comming" movies={upComing} />
+      </View>
+    </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+  },
+  carouselContainer: {
+    height: 440,
+  },
+});
 
 export default HomeScreen;
